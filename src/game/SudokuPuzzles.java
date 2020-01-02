8a96f0163d70c7d58a9ce8b7fc86911245fe840e
Shawn Kaplan <shawn.s.kaplan@gmail.com>
2020-01-02 04:24:58 -0500

    Added a third puzzle

--- src/game/SudokuPuzzles.java
+++ src/game/SudokuPuzzles.java
@@ -2,7 +2,9 @@ package game;
 
 public enum SudokuPuzzles {
 
-   PUZZLE1(getPuzzle1()), PUZZLE2(getPuzzle2());
+    PUZZLE1(getPuzzle1()),
+    PUZZLE2(getPuzzle2()),
+    PUZZLE3(getPuzzle3());
 
     private int[][] puzzle;
 
@@ -53,6 +55,20 @@ public enum SudokuPuzzles {
         thePuzzle[6] = new int[] {0, 0, 9, 3, 0, 0, 0, 7, 4};
         thePuzzle[7] = new int[] {0, 4, 0, 0, 5, 0, 0, 3, 6};
         thePuzzle[8] = new int[] {7, 0, 3, 0, 1, 8, 0, 0, 0};
+        return thePuzzle;
+    }
+
+    private static int[][] getPuzzle3() {
+        int[][] thePuzzle = new int[9][9];
+        thePuzzle[0] = new int[] {0, 0, 0, 6, 0, 0, 4, 0, 0};
+        thePuzzle[1] = new int[] {7, 0, 0, 0, 0, 3, 6, 0, 0};
+        thePuzzle[2] = new int[] {0, 0, 0, 0, 9, 1, 0, 8, 0};
+        thePuzzle[3] = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        thePuzzle[4] = new int[] {0, 5, 0, 1, 8, 0, 0, 0, 3};
+        thePuzzle[5] = new int[] {0, 0, 0, 3, 0, 6, 0, 4, 5};
+        thePuzzle[6] = new int[] {0, 4, 0, 2, 0, 0, 0, 6, 0};
+        thePuzzle[7] = new int[] {9, 0, 3, 0, 0, 0, 0, 0, 0};
+        thePuzzle[8] = new int[] {0, 2, 0, 0, 0, 0, 1, 0, 0};
         return thePuzzle;
     }
 }
