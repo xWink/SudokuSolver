2af713086f6151c7aa1573127878e73315db13dd
Shawn Kaplan <shawn.s.kaplan@gmail.com>
2020-01-02 03:37:47 -0500

    Added SudokuPuzzles.java to determine the puzzles for the board

--- src/game/SudokuGame.java
+++ src/game/SudokuGame.java
@@ -3,28 +3,20 @@ package game;
 public class SudokuGame {
 
     private SudokuBoard board;
+    private SudokuPuzzles puzzle;
 
     public SudokuGame() {
         reset();
     }
 
     public void reset() {
+        puzzle = SudokuPuzzles.values()[(int) (Math.random() * SudokuPuzzles.values().length)];
         board = new SudokuBoard();
         setNewValues();
     }
 
     private void setNewValues() {
-        int[][] values = new int[board.getBoard().length][board.getBoard()[0].length];
-        values[0] = new int[]{5, 3, 0, 0, 7, 0, 0, 0, 0};
-        values[1] = new int[]{6, 0, 0, 1, 9, 5, 0, 0, 0};
-        values[2] = new int[]{0, 9, 8, 0, 0, 0, 0, 6, 0};
-        values[3] = new int[]{8, 0, 0, 0, 6, 0, 0, 0, 3};
-        values[4] = new int[]{4, 0, 0, 8, 0, 3, 0, 0, 1};
-        values[5] = new int[]{7, 0, 0, 0, 2, 0, 0, 0, 6};
-        values[6] = new int[]{0, 6, 0, 0, 0, 0, 2, 8, 0};
-        values[7] = new int[]{0, 0, 0, 4, 1, 9, 0, 0, 5};
-        values[8] = new int[]{0, 0, 0, 0, 8, 0, 0, 7, 9};
-        board.setAllValues(values);
+        board.setAllValues(puzzle.getPuzzle());
     }
 
     public void setValue(int row, int column, int value) {
